083e47ced81eaa044661315fe2a31789703814a3
labuser <labuser@ip-172-31-47-230.ap-south-1.compute.internal>
2025-12-18 10:24:09 +0000

    my opentofu trainings

---
+++ lession1/main.tf
@@ -0,0 +1,9 @@
+resource "local_file" "hello_world" {
+  filename = "${path.module}/demo.txt"
+ 
+  content = <<-EOF
+    Hello World!!!
+    Welcome to the fascinating world of OpenTofu!!
+  EOF
+}
+ 
